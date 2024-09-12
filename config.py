b58b65e40118aeaf8d8087b50f9078ef8ef92737
totaflix <starastar230@gmail.com>
2024-09-12 14:47:30 +0600

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,10 @@
 
 from os import getenv
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
-MONGO_DB = getenv("MONGO_DB", "")
-LOG_GROUP = getenv("LOG_GROUP", "")
-CHANNEL_ID = int(getenv("CHANNEL_ID", ""))
+API_ID = int(getenv("API_ID", "22920744"))
+API_HASH = getenv("API_HASH", "31cb93c017f265e4fa6d0ba91236b826")
+BOT_TOKEN = getenv("BOT_TOKEN", "7018409603:AAHqvNE5c1HBJNG7RPT9aZAa03QRO67EzNw")
+OWNER_ID = list(map(int, getenv("OWNER_ID", "5659668981").split()))
+MONGO_DB = getenv("MONGO_DB", "mongodb+srv://starastar230:<password>@cluster0.5mhoa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+LOG_GROUP = getenv("LOG_GROUP", "-1002198037301")
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002278038477"))
